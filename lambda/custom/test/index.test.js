459b4b85a8d625b69dae8da9c8a32238862e507a
Nick Todd <nick.todd@conygre.com>
2019-05-08 14:40:26 +0100

    Fixed broken unit test

--- lambda/custom/test/index.test.js
+++ lambda/custom/test/index.test.js
@@ -2,15 +2,16 @@ const va = require("virtual-alexa");
 let alexa = null;
 
 beforeAll(() => {
-    alexa = va.VirtualAlexa.Builder()
+  alexa = va.VirtualAlexa.Builder()
     .handler("./index.handler") // Lambda function file and name
     .interactionModelFile("../../models/en-US.json") // Path to interaction model file
     .create();
 });
 
-it('should return hello Nick if Nick is the name provided', async () => {
-    payload = await alexa.utter("my name is Nick");//.then((payload) => {
-    console.log("OutputSpeech: " + payload.response.outputSpeech.ssml);
-    expect((payload.response.outputSpeech.ssml).toLowerCase())
-            .toContain("hello sarah");
+it("should return hello Nick if Nick is the name provided", async () => {
+  payload = await alexa.utter("my name is Nick"); //.then((payload) => {
+  console.log("OutputSpeech: " + payload.response.outputSpeech.ssml);
+  expect(payload.response.outputSpeech.ssml.toLowerCase()).toContain(
+    "hello Nick"
+  );
 });
